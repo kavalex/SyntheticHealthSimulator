7eeb0d2fa898113d400bea0fed086d0e49ffd24f
Alexey Kaverzin <ASKaverzin@alfabank.ru>
2026-02-27 17:22:43 +0300

    Bump version to 1.0.1

--- generator.py
+++ generator.py
@@ -18,8 +18,8 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=pd.errors.PerformanceWarning)
 
-__version__ = "1.0.0"
-__version_info__ = (1, 0, 0)
+__version__ = "1.0.1"
+__version_info__ = (1, 0, 1)
 
 
 class DataGenerator:
